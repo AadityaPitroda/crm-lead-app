b847d493bd6cc26b6574177ee00fb453f102d2fd
Aaditya <aadityapitroda@gmail.com>
2026-05-20 11:04:14 +0530

    Fix duplicate env identifier in backend entrypoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,19 @@
+// server/src/index.js
+
+// Optional crypto polyfill if you previously added it
+import * as nodeCrypto from 'node:crypto';
+if (!globalThis.crypto) {
+    globalThis.crypto = nodeCrypto;
+}
+
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+
 import { env } from './config/env.js';
 import { connectDB } from './config/db.js';
 import routes from './routes/index.js';
 import { errorHandler } from './middleware/error.middleware.js';
-import { env } from './config/env.js';
 
 const app = express();
 
@@ -19,7 +27,7 @@ app.get('/api/health', (req, res) => {
 
 app.use('/api', routes);
 
-// Error handler last
+// Error handler must be last
 app.use(errorHandler);
 
 async function start() {
